14bb114606b201a82fde79f413140f4a5aa07beb
Evan Dixon <evanldixon@live.com>
2016-06-12 14:15:56 -0500

    Added RelayCommand

---
+++ SkyEditor.Core/UI/RelayCommand.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Windows.Input
+
+Namespace UI
+    Public Class RelayCommand
+        Implements ICommand
+
+        Private ReadOnly _Execute As Action(Of Object)
+
+        Public Sub New(execute As Action(Of Object))
+            If execute Is Nothing Then
+                Throw New ArgumentNullException(NameOf(execute))
+            End If
+            _Execute = execute
+        End Sub
+
+        Public Event CanExecuteChanged As EventHandler Implements ICommand.CanExecuteChanged
+
+        Public Property IsEnabled() As Boolean
+            Get
+                Return _isEnabled
+            End Get
+            Set(ByVal value As Boolean)
+                If (value <> _isEnabled) Then
+                    _isEnabled = value
+                    RaiseEvent CanExecuteChanged(Me, EventArgs.Empty)
+                End If
+            End Set
+        End Property
+        Dim _isEnabled As Boolean
+
+        Public Function CanExecute(parameter As Object) As Boolean Implements ICommand.CanExecute
+            Return IsEnabled
+        End Function
+
+        Public Sub Execute(parameter As Object) Implements ICommand.Execute
+            _Execute.Invoke(parameter)
+        End Sub
+
+    End Class
+End Namespace
