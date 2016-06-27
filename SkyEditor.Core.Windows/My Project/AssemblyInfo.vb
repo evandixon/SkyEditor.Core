5e4e5d5845091f3e17cedd14ad622f38426be2e9
Evan Dixon <evanldixon@live.com>
2016-06-27 17:10:57 -0500

    Updated AssemblyInfo

--- SkyEditor.Core.Windows/My Project/AssemblyInfo.vb
+++ SkyEditor.Core.Windows/My Project/AssemblyInfo.vb
@@ -8,8 +8,8 @@ Imports System.Runtime.InteropServices
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("SkyEditor.Core.Windows")> 
-<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyTitle("SkyEditor.Core.Windows")>
+<Assembly: AssemblyDescription("Windows implementation of certain SkyEditor.Core classes.")>
 <Assembly: AssemblyCompany("")> 
 <Assembly: AssemblyProduct("SkyEditor.Core.Windows")> 
 <Assembly: AssemblyCopyright("Copyright ©  2016")> 
@@ -18,7 +18,7 @@ Imports System.Runtime.InteropServices
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("15fa3755-372c-4ed8-9c09-1675ba60cca5")> 
+<Assembly: Guid("15fa3755-372c-4ed8-9c09-1675ba60cca5")>
 
 ' Version information for an assembly consists of the following four values:
 '
@@ -31,5 +31,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
+<Assembly: AssemblyVersion("4.0.3.0")>
+<Assembly: AssemblyFileVersion("4.0.3.0")>
