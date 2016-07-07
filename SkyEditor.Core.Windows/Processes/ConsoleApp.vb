fd076312dcf5fdfd8a6ec2c353bd76e70b49f019
Evan Dixon <evanldixon@live.com>
2016-07-07 17:52:34 -0500

    Added RunProgramNoOutput

--- SkyEditor.Core.Windows/Processes/ConsoleApp.vb
+++ SkyEditor.Core.Windows/Processes/ConsoleApp.vb
@@ -53,6 +53,24 @@ Namespace Processes
             p.Dispose()
         End Function
 
+        Public Shared Async Function RunProgramNoOutput(Filename As String, Arguments As String) As Task
+            Using p As New Process()
+                p.StartInfo.FileName = Filename
+                p.StartInfo.Arguments = Arguments
+                p.StartInfo.RedirectStandardOutput = False
+                p.StartInfo.UseShellExecute = True
+                p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden
+                p.StartInfo.CreateNoWindow = True
+                p.StartInfo.WorkingDirectory = Path.GetDirectoryName(Filename)
+
+                'Start the process
+                p.Start()
+
+                'Wait for the process to close
+                Await WaitForProcess(p)
+            End Using
+        End Function
+
         ''' <summary>
         ''' Waits for the given process to exit.
         ''' </summary>
