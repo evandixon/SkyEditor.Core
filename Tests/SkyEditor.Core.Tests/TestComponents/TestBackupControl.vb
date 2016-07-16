a58de70c9ebe9a80167cfada31ab706edac7fb17
Evan Dixon <evanldixon@live.com>
2016-07-16 08:35:21 -0500

    Added tests for backup control support

---
+++ Tests/SkyEditor.Core.Tests/TestComponents/TestBackupControl.vb
@@ -0,0 +1,21 @@
+﻿Imports System.Reflection
+
+Namespace TestComponents
+    Public Class TestBackupControl
+        Inherits TestObjectControlDirectModelBind
+
+        Public Overrides Function GetSupportedTypes() As IEnumerable(Of Type)
+            Return {GetType(TextFile).GetTypeInfo}
+        End Function
+
+        Public Overrides Function SupportsObject(Obj As Object) As Boolean
+            Return TypeOf Obj Is TextFile
+        End Function
+
+        Public Overrides Function IsBackupControl(Obj As Object) As Boolean
+            Return True
+        End Function
+
+    End Class
+
+End Namespace
