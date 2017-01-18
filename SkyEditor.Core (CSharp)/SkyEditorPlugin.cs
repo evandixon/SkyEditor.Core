583350ede4bd3112b0190fbfc87c5e0cf7d0cc15
Evan Dixon <evanldixon@live.com>
2017-01-17 22:27:43 -0600

    NWC: started rewriting SkyEditor.Core in C# to use .Net Core

---
+++ SkyEditor.Core (CSharp)/SkyEditorPlugin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkyEditor.Core
+{
+    public abstract class SkyEditorPlugin
+    {
+        public SkyEditorPlugin()
+        {
+        }
+
+        public abstract string PluginName { get; }
+
+        public abstract string PluginAuthor { get; }
+        public abstract string Credits { get; }
+        public virtual void Load(PluginManager manager)
+        {
+
+        }
+    }
+}
