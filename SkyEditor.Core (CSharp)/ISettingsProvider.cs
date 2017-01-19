61a94a9790cb48a3e254801c01223f7ef613157d
Evan Dixon <evanldixon@live.com>
2017-01-19 11:44:12 -0600

    Added ISettingsProvider

---
+++ SkyEditor.Core (CSharp)/ISettingsProvider.cs
@@ -0,0 +1,28 @@
+﻿using SkyEditor.Core.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkyEditor.Core
+{
+    /// <summary>
+    /// Represents a class that can get and set values of settings
+    /// </summary>
+    public interface ISettingsProvider : ISavable
+    {
+        /// <summary>
+        /// Gets the value of a setting
+        /// </summary>
+        /// <param name="name">Name of the desired setting</param>
+        /// <returns>The value of the setting with the given name or null if the setting does not have a value</returns>
+        object GetSetting(string name);
+
+        /// <summary>
+        /// Sets a setting
+        /// </summary>
+        /// <param name="name">Name of the desired setting</param>
+        /// <param name="value">New value of the setting</param>
+        void SetSetting(string name, object value);
+    }
+}
