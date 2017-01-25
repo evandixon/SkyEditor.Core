17b3743e8e1f5689d0fb0e2ba03cbba0a59b0948
Evan Dixon <evanldixon@live.com>
2017-01-24 18:21:38 -0600

    Set up .net core test project

---
+++ Tests/SkyEditor.Core.Tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkyEditor.Core.Tests
+{
+    [TestClass]
+    public class Program
+    {
+        [TestMethod] public void Testing()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
