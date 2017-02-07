c9fabb818ec4e35f7e6279ae4808302fa09b909c
Evan Dixon <evanldixon@live.com>
2017-02-07 09:16:16 -0600

    Added MenuItemInfo

---
+++ SkyEditor.Core/UI/MenuItemInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace SkyEditor.Core.UI
+{
+    /// <summary>
+    /// Container for information used to make menu items (<see cref="ActionMenuItem"/>) from menu actions (<see cref="MenuAction"/>)
+    /// </summary>
+    public class MenuItemInfo
+    {
+        public MenuItemInfo()
+        {
+            this.ActionTypes = new List<TypeInfo>();
+            this.Children = new List<MenuItemInfo>();
+            this.SortOrder = 0;
+        }
+        public string Header { get; set; }
+        public List<TypeInfo> ActionTypes { get; set; }
+        public List<MenuItemInfo> Children { get; set; }
+        public decimal SortOrder { get; set; }
+    }
+}
