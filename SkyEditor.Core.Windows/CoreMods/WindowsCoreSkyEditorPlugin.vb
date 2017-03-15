ac9fd0ae7dfc80118ead5e2e93a90a75e32827e9
Evan Dixon <evanldixon@live.com>
2017-03-15 17:37:16 -0500

    Added reference to AssemblyLoadContext

--- SkyEditor.Core.Windows/CoreMods/WindowsCoreSkyEditorPlugin.vb
+++ SkyEditor.Core.Windows/CoreMods/WindowsCoreSkyEditorPlugin.vb
@@ -7,14 +7,6 @@ Namespace CoreMods
     Public MustInherit Class WindowsCoreSkyEditorPlugin
         Inherits CoreSkyEditorPlugin
 
-        Public Overrides Function GetIOProvider() As IIOProvider
-            Return New PhysicalIOProvider
-        End Function
-
-        Public Overrides Function GetConsoleProvider() As IConsoleProvider
-            Return New StandardConsoleProvider
-        End Function
-
         Public Overrides Sub Load(manager As PluginManager)
             MyBase.Load(manager)
 
@@ -22,20 +14,8 @@ Namespace CoreMods
             manager.RegisterType(Of ConsoleCommand, GeneratePluginExtensions)()
         End Sub
 
-        Public Overrides Function IsPluginLoadingEnabled() As Boolean
-            Return True
-        End Function
-
         Public Overrides Function LoadAssembly(assemblyPath As String) As Assembly
             Return WindowsReflectionHelpers.LoadAssembly(assemblyPath)
-        End Function
-
-        Public Overrides Function GetSettingsProvider(manager As PluginManager) As ISettingsProvider
-            Return SettingsProvider.Open(EnvironmentPaths.GetSettingsFilename, manager)
-        End Function
-
-        Public Overrides Function GetExtensionDirectory() As String
-            Return EnvironmentPaths.GetExtensionDirectory
         End Function
 
     End Class
